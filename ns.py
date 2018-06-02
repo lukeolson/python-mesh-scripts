9c0aedf6c82527a295f6d4d0b3c6bfbddc1134dc
Luke Olson <luke.olson@gmail.com>
2018-06-02 08:20:20 -0500

    clean up ns

--- ns.py
+++ ns.py
@@ -1,5 +1,4 @@
 import numpy as np
-import scipy as sp
 import matplotlib.pyplot as plt
 
 
@@ -18,6 +17,7 @@ def readmesh(fname):
             element ids
     """
     import gmsh
+
     mesh = gmsh.Mesh()
     mesh.read_msh(fname)
     return mesh.Verts[:, :2], mesh.Elmts[2][1]
@@ -38,54 +38,42 @@ def identify_boundary(V):
     """
     d = {}
 
-    I = np.where(np.abs(V[:, 0]) < 1e-13)
-    d['inflow'] = I
+    II = np.where(np.abs(V[:, 0]) < 1e-13)
+    d["inflow"] = II
 
-    I = np.where(np.abs(V[:, 0] - 22.0) < 1e-13)
-    d['outflow'] = I
+    II = np.where(np.abs(V[:, 0] - 22.0) < 1e-13)
+    d["outflow"] = II
 
-    I = np.where(np.abs(V[:, 1]) < 1e-13)
+    II = np.where(np.abs(V[:, 1]) < 1e-13)
     J = np.where(np.abs(V[:, 1] - 4.1) < 1e-13)
-    d['wall'] = np.vstack((I, J)).ravel()
+    d["wall"] = np.vstack((II, J)).ravel()
 
-    I = np.where(np.abs(2 *
-                        np.sqrt((V[:, 0] - 2.0)**2 + (V[:, 1] - 2.0)**2)
-                        - 1.0) < 1e-13)
-    d['cylinder'] = I
+    II = np.where(
+        np.abs(2 * np.sqrt((V[:, 0] - 2.0) ** 2 + (V[:, 1] - 2.0) ** 2) - 1.0) < 1e-13
+    )
+    d["cylinder"] = II
 
     return d
 
 
-def local_matrices():
-    """
-    create some local matrices
-    """
-    pass
+if __name__ == "__main__":
 
-def assemble_fem()
-    """
-    assemble a LS FEM
-    """
-    pass
-
-if __name__ == '__main__':
-
-    V, E = readmesh('ns.msh')
+    V, E = readmesh("ns.msh")
     d = identify_boundary(V)
 
     plt.ion()
     plt.triplot(V[:, 0], V[:, 1], E)
-    plt.axis('scaled')
+    plt.axis("scaled")
 
-    I = d['inflow']
-    plt.plot(V[I, 0], V[I, 1], 'ro', markersize=10)
+    II = d["inflow"]
+    plt.plot(V[II, 0], V[II, 1], "ro", markersize=10)
 
-    I = d['outflow']
-    plt.plot(V[I, 0], V[I, 1], 'bo', markersize=10)
+    II = d["outflow"]
+    plt.plot(V[II, 0], V[II, 1], "bo", markersize=10)
 
-    I = d['wall']
-    plt.plot(V[I, 0], V[I, 1], 'gs', markersize=10)
+    II = d["wall"]
+    plt.plot(V[II, 0], V[II, 1], "gs", markersize=10)
 
-    I = d['cylinder']
-    plt.plot(V[I, 0], V[I, 1], 'm*', markersize=10)
+    II = d["cylinder"]
+    plt.plot(V[II, 0], V[II, 1], "m*", markersize=10)
     plt.show()
